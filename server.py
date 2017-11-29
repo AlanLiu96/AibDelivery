f4a20d8a52e0d734eba54a8a203c6228192d8f6f
Alan Liu <alanl.liu@yahoo.com>
2017-11-28 21:36:59 -0500

    float example

--- server.py
+++ server.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('float.html')
+
+# @app.route("/name/<language>")
+# def name(language):
+# 	if language == 'EN':
+# 		return render_template('english_name.html')
+# 	else:
+#     	return render_template('home.html')
+
+# @app.route("/email")
+# def email():
+#     return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
